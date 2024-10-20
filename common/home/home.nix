a91682450e460f9b453088761a65e4e484595aef
Turizmo <stianfoss.90+github@gmail.com>
2024-10-20 20:18:48 +0200

    Added instruction to ssh keys for github

--- common/home/home.nix
+++ common/home/home.nix
@@ -65,4 +65,11 @@
     userName = "Turizmo";
     userEmail = "stianfoss.90+github@gmail.com";
   };
+  # For pushing to github you need to configure an SSH key manually
+  # Check if you already have a generated key by lookin for this file: ~/.ssh/id_rsa.pub
+  # If the key does not exist you can generate a key by running this command in the terminal: ssh-keygen -t rsa -C "description"
+  # Copy the the public key(~/.ssh/id_rsa.pub) into github (profile -> settings -> SSH and GPG keys -> New SSH key)
+  # NB! Never share the private key(~/.ssh/id_rsa)
+  # Add the private key to the ssh agent by running this command in the terminal: ssh-add ~/.ssh/id_rsa
+  # The first push must be done in the terminal afterwards you can use lazygit
 }
