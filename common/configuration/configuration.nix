964788b753d021016fa9a01bca5a38e649624c1a
Turizmo <stianfoss.90+github@gmail.com>
2024-12-03 12:52:40 +0100

    Added plasma for debugging

--- common/configuration/configuration.nix
+++ common/configuration/configuration.nix
@@ -62,7 +62,7 @@
   #  };
   #	services.xserver.displayManager.gdm.enable = true;
 
-  # services.desktopManager.plasma6.enable = true;
+   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
